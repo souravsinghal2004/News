8d0d11a6421f1c6d38606d618ce57e62d08a46c4
SOURAV SINGHAL <souravsinghal2004@gmail.com>
2025-08-05 11:20:14 +0530

    fixing prod

--- app/page.jsx
+++ app/page.jsx
@@ -12,9 +12,8 @@ export default async function Page() {
 
   // ✅ Dynamically set base URL for server-side fetch
   const baseUrl =
-    process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000";
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
 
   function delay(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
